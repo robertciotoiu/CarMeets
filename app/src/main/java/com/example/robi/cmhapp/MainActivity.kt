13d85170f8275eff93ef8ef5e04e84e4efe5760f
robertciotoiu <robertciotoiu@users.noreply.github.com>
2018-10-15 15:37:11 +0300

    InitialCommit

---
+++ app/src/main/java/com/example/robi/cmhapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.robi.cmhapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        hideStatusBar()
+
+    }
+
+    private fun hideStatusBar()
+    {
+        setContentView(R.layout.activity_main)
+        // Hide the status bar.
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        actionBar?.hide()
+    }
+
+}
